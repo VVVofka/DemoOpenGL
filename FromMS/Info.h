ae8e631bf304265418b5af7377f89acfb5e54e07
vvvofka@yandex.ru <vvvofka@yandex.ru>
2019-10-20 10:35:55 +0300

    add info.h

---
+++ FromMS/Info.h
@@ -0,0 +1,8 @@
+#pragma once
+/*
+from https://docs.microsoft.com/en-us/windows/win32/opengl/the-program-ported-to-win32
+Create new project -> Windows DeskTop Wizard -> Desktop Application (exe) & Empty project
+Project Property:
+	Advanced -> Character Set -> Use Multi Character set
+	Linker -> Input-> Additional Dependence -> add OPENGL32.LIB GLU32.LIB
+ */
---
+++ OG/Info.h
@@ -0,0 +1,10 @@
+#pragma once
+/*
+eng:
+tutorial update http://www.opengl-tutorial.org/
+
+rus:
+old, 4 parts https://www.helloworld.ru/texts/comp/games/opengl/opengl/1.htm
+Объёмный учебник 2005 https://www.rsdn.org/article/opengl/ogltutor.xml
+small 2D  http://www.cyberforum.ru/opengl/thread569240.html https://habr.com/ru/post/111175/
+*/
